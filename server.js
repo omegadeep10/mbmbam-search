2061f121490e5d3e26f0e5fa0ddcc5bb2c641b30
Deep Patel <dpatel@stins.com>
2016-12-20 10:14:58 -0500

    Fine tune fuse.js search parameters and data sanitization

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ var port = process.env.PORT || 8085;
 
 //Routes
 var router = express.Router();
+
 //every route is under the /api/ endpoint
 app.use('/api', router);
 
@@ -37,30 +38,36 @@ router.get('/:searchTerm', function(req, res) {
         var data = [];
 
         $("#results_table tbody").children().each(function(index, element) {
+            
+            var desc = ent.decode($(element).find("td:nth-child(3)").html()).replace(/\n/g, ""); //remove line breaks
+            var talking_points = desc.match(/Suggested talking points:(.*)/i);
+
+            //if talking points exist in desc, format data and return array of talking points
+            if (talking_points) talking_points = talking_points[1].trim().split(',').map((x) => { return x.trim(); });
+
+
+            var ep_title = ent.decode($(element).find("td > a").html());
+            var data_title = ep_title.match(/:(.*)/);
+
             data.push({
-                title: ent.decode($(element).find("td > a").html()),
+                ep_title: ep_title,
+                title: data_title ? data_title[1].trim() : "",
                 date: $(element).find("td:nth-child(2)").html(),
-                description: ent.decode($(element).find("td:nth-child(3)").html())
+                talking_points: talking_points ? talking_points : "",
+                description: desc
             });
         });
 
         var options = {
+            include: ["score", "matches"],
             shouldSort: true,
-            threshold: 0.6,
-            tokenize: true,
+            threshold: 0.25,
             findAllMatches: true,
             location: 0,
             distance: 100,
             maxPatternLength: 32,
-            minMatchCharLength: 1,
-            keys: [{
-                name: "title",
-                weight: 0.7
-            }, {
-                name: "description",
-                weight: 0.5
-            }
-            ]
+            minMatchCharLength: 3,
+            keys: ["title", "talking_points"]
         };
         var fuse = new Fuse(data, options);
         var result = fuse.search(req.params.searchTerm);
